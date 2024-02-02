ddfb12977e77f842644781ab4936edbf0ce68d7a
xaluoqone <1617321643@qq.com>
2024-02-02 15:57:10 +0800

    resumes 改为后端获取

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,85 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent } from 'react'
 import './App.css'
+import { useMount, useReactive } from 'ahooks'
 
 interface Doc {
-  title: string
+  name: string
   url: string
 }
 
 interface Resume extends Doc {
+  id: number
 }
-
-const resumes: Resume[] = [
-  {
-    title: '个人简历-2023',
-    url: '/resume-2023.pdf',
-  },
-  {
-    title: '个人简历-2024',
-    url: '/resume-2024.pdf',
-  },
-]
 
 const docs: Doc[] = [
   {
-    title: 'Retrofit 源码解析.md',
+    name: 'Retrofit 源码解析.md',
     url: 'https://github.com/xaluoqone/xaluoqone.com/blob/dev/public/docs/http/Retrofit%20%E6%BA%90%E7%A0%81%E8%A7%A3%E6%9E%90.md',
   },
 ]
 
 function App() {
+  const state = useReactive({
+    resumes: [] as Resume[],
+    file: undefined as File | undefined,
+  })
+
+  useMount(() => {
+    fetch('https://api.xaluoqone.com/resume/all', { method: 'GET' })
+      .then(async response => {
+        if (response.ok) {
+          state.resumes = await response.json()
+        } else {
+          console.error('获取简历列表失败')
+        }
+      })
+      .catch(error => console.error('Error:', error))
+  })
+
   return (
     <div className={'root'}>
       <div className={'content'} style={{ flexDirection: 'column' }}>
         <h3>Welcome to xaluoqone.com</h3>
         <ul><h4>简历列表</h4>{
-          resumes.map(resume => (
-            <li>
-              <a href={resume.url} target={'_blank'} rel={'noreferrer'}>{resume.title}</a>
+          state.resumes.map(resume => (
+            <li key={resume.id}>
+              <a href={resume.url} target={'_blank'} rel={'noreferrer'}>{resume.name}</a>
             </li>
           ))
         }</ul>
         <ul><h4>小记</h4>{
           docs.map(doc => (
-            <li>
-              <a href={doc.url} target={'_blank'} rel={'noreferrer'}>{doc.title}</a>
+            <li key={doc.url}>
+              <a href={doc.url} target={'_blank'} rel={'noreferrer'}>{doc.name}</a>
             </li>
           ))
         }</ul>
+        <form
+          onSubmit={(event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault()
+            if (!state.file) return
+            const formData = new FormData()
+            formData.append('file', state.file)
+            fetch('https://api.xaluoqone.com/resume/upload', { method: 'POST', body: formData })
+              .then(response => {
+                if (response.ok) {
+                  console.log('File uploaded successfully')
+                } else {
+                  console.error('File upload failed')
+                }
+              })
+              .catch(error => console.error('Error:', error))
+          }}>
+          <input
+            type="file"
+            name="file"
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              if (event.target.files && event.target.files.length > 0) {
+                state.file = event.target.files[0]
+              }
+            }}/>
+          <button type="submit">Upload</button>
+        </form>
       </div>
       <div className={'footer'}>
         <a href="https://beian.miit.gov.cn">赣ICP备2021006394号</a>
